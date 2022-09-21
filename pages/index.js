edb3711b67943e2830f8759a96d418fb5c4d35b1
MuqtadirBillah <abbasimuqtadir@gmail.com>
2022-09-21 15:28:31 +0500

    added dynamic header for seo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import DynamicHead from '../src/components/DynamicHead'
 
 export default function Home() {
   return (
     <div>
-      
+      <DynamicHead title="Todo List" description="Manage your Todo list with multiple folders." keywords="todo list" />
     </div>
   )
 }
