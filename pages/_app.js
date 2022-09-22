5d22c2eb3ca7adbe93589d14356bcafa26eaa777
MuqtadirBillah <abbasimuqtadir@gmail.com>
2022-09-23 00:04:15 +0500

    added clarity

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,21 @@ function MyApp({ Component, pageProps }) {
           draggable
           pauseOnHover
       />
+      <script
+        dangerouslySetInnerHTML={
+                      {
+          __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a] = c[a] || function () { (c[a].q = c[a].q || 
+                  []).push(arguments) };
+                  t=l.createElement(r);
+                  t.async=1;
+                  t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];
+                  y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "drqe91y2o2");`,
+        }}
+      />
       <Component {...pageProps} />
     </>
   )
